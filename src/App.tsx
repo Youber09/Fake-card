ea37db0008214b009a772d1bf12ad9ac4b3fbebc
Youber09 <youber09@gmail.com>
2025-11-30 22:58:55 +0100

    da change in height

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ const App = () => {
       <h1 className="text-[6vw] mukta-bold m-[3%] ml-[5%]">Setram Card</h1>
 
       <div
-        className="w-full h-full p-[3%] mt-[-5%]"
+        className="w-full h-full p-[3%] mt-[-2.5%]"
         ref={containerRef}
         onPointerDown={handlePointerDown}
         onPointerMove={handlePointerMove}
